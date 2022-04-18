7a72679c571306377ca007a1595b1885d7509a42
alfa23 <paw235@gmail.com>
2022-04-18 18:23:07 +0000

    add: js to check and return api key status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,34 @@
-//alfa23 jshint api key: _gpFEYqtN_YILLHofhst-uvjc_w
+const API_KEY = "_gpFEYqtN_YILLHofhst-uvjc_w";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
 
 // //header setting with key:
 // const response = fetch("https://ci-jshint.herokuapp.com/api", {
