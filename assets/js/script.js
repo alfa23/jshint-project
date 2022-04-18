cda7bcad371576be1c644b824eb9e88c9e6e2f29
alfa23 <paw235@gmail.com>
2022-04-18 12:12:31 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//alfa23 jshint api key: _gpFEYqtN_YILLHofhst-uvjc_w
+
+// //header setting with key:
+// const response = fetch("https://ci-jshint.herokuapp.com/api", {
+//     method: "POST",
+//     headers: {
+//         "Authorization": _gpFEYqtN_YILLHofhst-uvjc_w,
+//     }
+// })
